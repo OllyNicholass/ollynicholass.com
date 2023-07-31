2f005a9a8add879a4691c2504e95842a4db81ff4
OllyNicholass <ollynicholassofficial@gmail.com>
2023-07-31 16:59:03 +0100

    feat(*): init sveltekit project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<slot />
+
+<style>
+  @import 'https://fonts.googleapis.com/css?family=Roboto&display=swap';
+  :global(body) {
+    font-family: 'Roboto', sans-serif;
+    background-color: #e09a3e;
+    color: #fff;
+    font-size: 2em;
+    margin: 0;
+  }
+</style>
