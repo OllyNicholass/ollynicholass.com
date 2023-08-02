e75bab8da389ad57e6987babf60301c0b12abee6
OllyNicholass <ollynicholassofficial@gmail.com>
2023-08-02 19:14:43 +0100

    feat(*): Add component/page for plans

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
+<script lang="ts">
+  import Jumbo from '$lib/components/home/+jumbo.svelte'
+</script>
+
+<Jumbo />
+
 {#each {length: 10} as _, i}
-<div class="center" data-iteration={i}>
+<div class="center mt-6" data-iteration={i}>
   <h1 class="h1">ollynicholass.com</h1>
   <h2 class="h2">coming soon</h2>
 </div>
