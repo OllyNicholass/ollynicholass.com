c86f0092cc9349a1f1c8b994fcfaf72b2bf949dd
OllyNicholass <ollynicholassofficial@gmail.com>
2023-09-03 15:26:33 +0100

    build: update tailwind config to use plugin and use esm module

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,20 @@
-import { join } from 'path'
+import { join } from 'path';
+import type { Config } from 'tailwindcss';
 import forms from '@tailwindcss/forms'
 import typography from '@tailwindcss/typography'
-import skeleton from '@skeletonlabs/skeleton/tailwind/skeleton.cjs'
+import { skeleton } from '@skeletonlabs/tw-plugin';
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
 	darkMode: 'class',
 	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
 	theme: {
 		extend: {},
 	},
-	plugins: [forms, typography, ...skeleton()],
-}
+	plugins: [
+		forms,
+		typography,
+		skeleton
+	]
+} satisfies Config;
+
+export default config;
