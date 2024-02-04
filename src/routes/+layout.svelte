6314802b75fba06eccaa12df1e2e3d9f7db65c01
OllyNicholass <OllyNicholass@users.noreply.github.com>
2024-02-04 21:04:13 +0000

    feat: add ga analytics and cookie consent banner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,26 @@
 <script lang="ts">
-  import '../app.postcss';
+	import '../app.postcss';
 	import Header from '$lib/components/header/+header.svelte';
 	import SkipContent from '$lib/components/header/+skip-content.svelte';
-	import { initializeStores, Drawer } from '@skeletonlabs/skeleton';
-	initializeStores();
+	import CookieConsent from '$lib/components/shared/CookieConsent.svelte';
 	let footerDateYear = new Date().getFullYear();
 </script>
 
 <svelte:head>
 	<title>olly | web development services</title>
-	<meta name="description" content="Enhance your online success with a seasoned freelance web developer, adept at crafting high-performance small business websites and handling complex commercial projects. With over a decade of experience, we specialize in top-notch hosting services and bespoke website building solutions to propel your online ventures to the forefront.">
-	<meta name="keywords" content="Freelance web developer, small business websites, experienced web developer, commercial web applications, larger-scale projects, hosting specialist, website building expert, online success, web development services">
+	<meta
+		name="description"
+		content="Enhance your online success with a seasoned freelance web developer, adept at crafting high-performance small business websites and handling complex commercial projects. With over a decade of experience, we specialize in top-notch hosting services and bespoke website building solutions to propel your online ventures to the forefront."
+	/>
+	<meta
+		name="keywords"
+		content="Freelance web developer, small business websites, experienced web developer, commercial web applications, larger-scale projects, hosting specialist, website building expert, online success, web development services"
+	/>
 </svelte:head>
 
 <SkipContent anchorId="main" />
-<Drawer />
 <Header />
+<CookieConsent />
 
 <div id="main" class="mb-4">
 	<slot />
@@ -31,9 +36,8 @@
 	:global(html) {
 		scroll-behavior: smooth;
 	}
-	
+
 	:global(body) {
 		margin: 0;
 	}
-	
 </style>
